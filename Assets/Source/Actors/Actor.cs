142d7820e6a849559ae1cb90b4f75f2a613709ac
Hubert <hubertkrzysztonekk@gmail.com>
2022-05-26 13:27:55 +0200

    123

--- Assets/Source/Actors/Actor.cs
+++ Assets/Source/Actors/Actor.cs
@@ -5,7 +5,7 @@ using DungeonCrawl.Actors.Characters;
 using DungeonCrawl.Actors.Static;
 using DungeonCrawl.Core;
 using UnityEngine;
-
+using Vector3 = UnityEngine.Vector3;
 
 namespace DungeonCrawl.Actors
 {
