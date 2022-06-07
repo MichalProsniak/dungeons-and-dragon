35f5850baf2e510352f1dfa425b810dafcf21be6
Damian Tarczoń <tarczon96@gmail.com>
2022-06-07 10:19:35 +0200

    DataManager connection class added

---
+++ Assets/Source/Core/DataManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using UnityEngine;
+
+namespace DungeonCrawl.Core
+{
+    public class DataManager : MonoBehaviour
+    {
+        public static DataManager Singleton { get; private set; }
+
+        public string ConnectionString => @"Server=localhost;Database=Dungeons;User Id=Damian;Password=Explorer1;";
+
+        private void Awake()
+        {
+            if (Singleton != null)
+            {
+                Destroy(this);
+                return;
+            }
+
+            Singleton = this;
+            Singleton.OpenConnection();
+        }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                using SqlConnection connection =
+                    new SqlConnection(ConnectionString);
+                connection.Open();
+                Debug.Log(connection.ServerVersion);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+    }
+}
